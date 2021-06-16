cc12d7c0d0cc171dc9d3bb548fa9353cacbb019f
Mateo <mateobetancurb@gmail.com>
2021-06-16 10:09:03 -0500

    se clonan archivos

---
+++ script.js
@@ -0,0 +1,18 @@
+window.sr = ScrollReveal();
+sr.reveal(".mi-foto", {
+    duration: 1000,
+    origin: "left",
+    distance: "300px",
+});
+
+sr.reveal(".primer-titulo", {
+    duration: 1000,
+    origin: "top",
+    distance: "300px",
+});
+
+sr.reveal(".bienvenida", {
+    duration: 2000,
+    origin: "top",
+    distance: "500px",
+});
